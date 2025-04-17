cd5f4bf58e87948b5b263667cdebf5f318576334
Murilo <murilomartins847@gmail.com>
2025-04-17 14:54:46 -0300

    Ajuste de finalização da calculadora

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 opcao = '';
 
 
-function mostrarValor(ev){
+function mostrarValor(ev) {
     console.log(ev.target.innerText);
 }
 
 
-function getInput(opcao){
-    if (opcao == 'nome'){
+function getInput(opcao) {
+    if (opcao == 'nome') {
         var nome = document.getElementById('input').value;
         return nome;
     }
-    else if (opcao == 'idade'){
+    else if (opcao == 'idade') {
         var idade = document.getElementById('input').value;
         return idade;
     }
 }
 
-function guardarNome() {  
-    console.log(getInput('nome')); 
-    document.getElementById('nomeDisplay').innerText = 'Nome: '+getInput('nome');
-  document.getElementById('input').value = '';
+function guardarNome() {
+    console.log(getInput('nome'));
+    document.getElementById('nomeDisplay').innerText = 'Nome: ' + getInput('nome');
+    document.getElementById('input').value = '';
 
 }
-function guardarIdade() {  
-  console.log(getInput('idade'));
-  document.getElementById('idadeDisplay').innerText = 'Idade: '+getInput('idade');
-  document.getElementById('input').value = '';
+function guardarIdade() {
+    console.log(getInput('idade'));
+    document.getElementById('idadeDisplay').innerText = 'Idade: ' + getInput('idade');
+    document.getElementById('input').value = '';
 }
 
 
-function limparVariaveis(){
+function limparVariaveis() {
     document.getElementById('nomeDisplay').innerText = 'Nome: ';
     document.getElementById('idadeDisplay').innerText = 'Idade: ';
 }
